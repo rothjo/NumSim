a7b7e6cd8c1f79ada01161799b4f1552d64c52cd
Julian <julian.eifert02@gmail.com>
2024-10-29 14:44:40 +0100

    disc

---
+++ src/discretization/discretization.h
@@ -0,0 +1,114 @@
+#pragma once
+
+#include <array>
+#include "StaggeredGrid.h"
+
+/**
+ * Calculate derivatives
+ */
+
+class Discretization : public StaggeredGrid {
+public:
+    Discretization(std::array<double,2> meshWidth, Settings settings);
+
+    /**
+     * Compute the derivative of u^2 in x-direction
+     * 
+     * @param i index in x-direction
+     * @param j index in y-direction
+     * @return derivative of u^2 in x-direction
+     */
+
+    virtual double computeDu2Dx (int i, int j) const;
+
+    /**
+     * Compute the derivative of u*v in x-direction
+     * 
+     * @param i index in x-direction
+     * @param j index in y-direction
+     * @return derivative of u*v in x-direction
+     */
+
+    virtual double computeDuvDx (int i, int j) const;
+
+    /**
+     * Compute the derivative of p in x-direction
+     * 
+     * @param i index in x-direction
+     * @param j index in y-direction
+     * @return derivative of p in x-direction
+     */
+
+    virtual double computeDpDx (int i, int j) const;
+
+    /**
+     * Compute the derivative of v^2 in y-direction
+     * 
+     * @param i index in x-direction
+     * @param j index in y-direction
+     * @return derivative of v^2 in y-direction
+     */
+
+    virtual double computeDv2Dy (int i, int j) const;
+
+    /**
+     * Compute the derivative of u*v in y-direction
+     * 
+     * @param i index in x-direction
+     * @param j index in y-direction
+     * @return derivative of u*v in y-direction
+     */
+
+    virtual double computeDuvDy (int i, int j) const;
+
+    /**
+     * Compute the derivative of p in y-direction
+     * 
+     * @param i index in x-direction
+     * @param j index in y-direction
+     * @return derivative of p in y-direction
+     */
+
+    virtual double computeDpDy (int i, int j) const;
+
+    /**
+     * Compute the second derivative of u in x-direction
+     * 
+     * @param i index in x-direction
+     * @param j index in y-direction
+     * @return second derivative of u in x-direction
+     */
+
+    virtual double computeD2uDx2 (int i, int j) const;
+
+    /**
+     * Compute the second derivative of u in y-direction
+     * 
+     * @param i index in x-direction
+     * @param j index in y-direction
+     * @return second derivative of u in y-direction
+     */
+
+    virtual double computeD2uDy2 (int i, int j) const;
+
+    /**
+     * Compute the second derivative of v in x-direction
+     * 
+     * @param i index in x-direction
+     * @param j index in y-direction
+     * @return second derivative of v in x-direction
+     */
+
+    virtual double computeD2vDx2 (int i, int j) const;
+
+    /**
+     * Compute the second derivative of v in y-direction
+     * 
+     * @param i index in x-direction
+     * @param j index in y-direction
+     * @return second derivative of v in y-direction
+     */
+
+    virtual double computeD2vDy2 (int i, int j) const;
+
+};
