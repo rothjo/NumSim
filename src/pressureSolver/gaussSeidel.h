813fd15efac2183b08744c87a10204192235fadb
jonas <jonas.roth@outlook.de>
2024-10-31 10:27:57 +0100

    gauss seidel branch

---
+++ src/pressureSolver/gaussSeidel.h
@@ -0,0 +1,19 @@
+#pragma once
+
+#include "pressureSolver.h"
+
+
+
+class GaussSeidel : public PressureSolver {
+public:
+    GaussSeidel(std::shared_ptr<Discretization>, double epsilon, int maximumNumberOfIterations);
+
+    /**
+     * Solve poisson problem for the pressure, using the rhs and p field variables in staggeredGrid
+     */
+    void solve() override;
+
+private:
+    void computeResidualNorm() override;
+};
+  
